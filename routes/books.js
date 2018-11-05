e930f7fc16165a4ecfa8f240a4200e01c23e1c42
Dan Dart <git@dandart.co.uk>
2018-11-05 21:20:28 +0000

    books route (todo)

---
+++ routes/books.js
@@ -0,0 +1,9 @@
+const Book = require('../models/book');
+const Institution = require('../models/institution');
+const User = require('../models/user');
+
+/**
+ * Retrieves books from a collection.
+ */
+module.exports = async (req, res) => {
+};
