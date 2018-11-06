02e0feead94787fb69b9d94f4358928dda262290
Dan Dart <git@dandart.co.uk>
2018-11-06 21:15:47 +0000

    fix books route

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ const Book = require('../models/book');
  * Retrieves books from a collection.
  */
 module.exports = async (req, res) => {
-  const user = req.session.user;
+  const user = req.session.passport.user;
   const institutionIds = user.institutions;
   res.status(200).json(
     await Book.find({
