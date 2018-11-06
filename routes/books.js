622863390e2cc3a7238011784984d7ad34cfedea
Dan Dart <git@dandart.co.uk>
2018-11-06 19:57:06 +0000

    flesh out books route

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,16 @@
 const Book = require('../models/book');
-const Institution = require('../models/institution');
-const User = require('../models/user');
 
 /**
  * Retrieves books from a collection.
  */
 module.exports = async (req, res) => {
+  const user = req.session.user;
+  const institutionIds = user.institutions;
+  res.status(200).json(
+    await Book.find({
+      institutions: {
+        $in: institutionIds
+      }
+    })
+  );
 };
